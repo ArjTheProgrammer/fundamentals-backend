a3f90e08e9178a94fe3732d3e3d0ef19e3d85c7e
Jose Neil Silagan <neilsilagan@gmail.com>
2025-05-04 15:28:41 +0800

    Create initial project structure

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const userRouter = require('./routes/users')
+const app = express()
+
+app.use(express.json())
+
+app.use('/users', userRouter)
+
+module.exports = app
---
+++ utils/config.js
@@ -0,0 +1,28 @@
+const sql = require('mssql')
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
+  options: {
+    trustServerCertificate: true, // for localhost dev
+  },
+}
+
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    console.log('Connected to MSSQL')
+    return pool
+  })
+  .catch(err => console.log('Database Connection Failed! Bad Config: ', err))
+
+module.exports = {
+  sql,
+  PORT,
+  poolPromise
+}

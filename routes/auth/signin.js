c221e32ad4091bbc677a36c4dcebc609794073e5
Jose Neil Silagan <neilsilagan@gmail.com>
2025-05-12 17:20:16 +0800

    Add signup for users and get users to validate

--- routes/auth/signin.js
+++ routes/auth/signin.js
@@ -1,20 +1,20 @@
-// const signupRouter = require('express').Router()
-// const config = require('../../utils/config')
+const signinRouter = require('express').Router()
+const config = require('../../utils/config')
 
-// signupRouter.post('/', async (req, res) => {
-//   const { firstname, lastname, username, email, password } = req.body
-//   try {
-//     const pool = await config.poolPromise
-//     const result = await pool.request()
-//       .input('password', config.sql.VarChar, password)
-//       .input('username', config.sql.VarChar, username)
-//       .query(`SELECT * FROM Users
-//         WHERE username = @username
-//         AND password = @password`)
-//     res.json(result.recordset)
-//   } catch (err) {
-//     res.status(500).send({ message: err.message })
-//   }
-// })
+signinRouter.post('/', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const pool = await config.poolPromise
+    const result = await pool.request()
+      .input('password', config.sql.VarChar, password)
+      .input('username', config.sql.VarChar, username)
+      .query(`SELECT * FROM Users
+        WHERE username = @username
+        AND password = @password`)
+    res.json(result.recordset)
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
+})
 
-// module.exports = signupRouter
+module.exports = signinRouter

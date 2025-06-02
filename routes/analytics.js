c652fcfaec84fe4d923486d88ed4660de94897b0
Jose Neil Silagan <neilsilagan@gmail.com>
2025-06-02 22:51:18 +0800

    Add analytics

--- routes/analytics.js
+++ routes/analytics.js
@@ -4,26 +4,24 @@ const config = require('../utils/config')
 analyticsRouter.get('/:id', async (req, res) => {
   try {
     const pool = await config.poolPromise
-    const result = await pool.request()
-      .input('userId', config.sql.Int, req.params.id)
-      .query(`
+    const result = await pool
+      .request()
+      .input('userId', config.sql.Int, req.params.id).query(`
         SELECT 
-    u.user_id,
-    u.first_name,
-    u.last_name,
-    SUM(CASE WHEN s.skill_name = 'Dribbling' THEN dt.completed_count ELSE 0 END) as dribbling_count,
-    SUM(CASE WHEN s.skill_name = 'Passing' THEN dt.completed_count ELSE 0 END) as passing_count,
-    SUM(CASE WHEN s.skill_name = 'Shooting' THEN dt.completed_count ELSE 0 END) as shooting_count,
-    SUM(CASE WHEN s.skill_name = 'Rebounding' THEN dt.completed_count ELSE 0 END) as rebounding_count,
-    SUM(CASE WHEN s.skill_name = 'Defense' THEN dt.completed_count ELSE 0 END) as defense_count,
-    SUM(dt.completed_count) as total_practice_count
-    FROM Users u
-    LEFT JOIN Drill_Tracking dt ON u.user_id = dt.user_id
-    LEFT JOIN Drills d ON dt.drill_id = d.drill_id
-    LEFT JOIN Skills s ON d.skill_id = s.skill_id
-    WHERE u.user_id = @userid
-    GROUP BY u.user_id, u.first_name, u.last_name;
-        `)
+          u.user_id,
+          u.first_name,
+          u.last_name,
+          SUM(CASE WHEN st.skill_id = 1 THEN st.completed_count ELSE 0 END) as dribbling_count,
+          SUM(CASE WHEN st.skill_id = 2 THEN st.completed_count ELSE 0 END) as passing_count,
+          SUM(CASE WHEN st.skill_id = 3 THEN st.completed_count ELSE 0 END) as shooting_count,
+          SUM(CASE WHEN st.skill_id = 4 THEN st.completed_count ELSE 0 END) as rebounding_count,
+          SUM(CASE WHEN st.skill_id = 5 THEN st.completed_count ELSE 0 END) as defense_count,
+          SUM(st.completed_count) as total_practice_count
+        FROM Users u
+        LEFT JOIN Skill_Tracking st ON u.user_id = st.user_id
+        WHERE u.user_id = @userId
+        GROUP BY u.user_id, u.first_name, u.last_name;
+      `)
     res.json(result.recordset)
   } catch (err) {
     res.status(500).send({ message: err.message })
